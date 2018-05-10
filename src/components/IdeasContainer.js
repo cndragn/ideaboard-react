ad9ca7fb5544ecb304b6084d9e6117ef7e31b061
Candice Davidson <cndragn@gmail.com>
2018-05-09 20:46:41 -0400

    create IdeasContainer component

---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class IdeasContainer extends Component {
+  render() {
+    return (
+        <div className="">
+            Ideas
+        </div>  
+    );
+  }
+}
+
+export default IdeasContainer
