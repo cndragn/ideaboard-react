c4ef2141d789a58c82f48070e724acc824815fd3
Candice Davidson <cndragn@gmail.com>
2018-05-10 01:20:14 -0400

    update database new idea form

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class IdeaForm extends Component {
     constructor(props) {
@@ -13,10 +14,23 @@ class IdeaForm extends Component {
         this.setState({[e.target.name]: e.target.value})
     }
 
+    handleBlur = () => {
+        const idea = {title: this.state.title, body: this.state.body}
+        axios.put(
+            `http://localhost:3001/api/v1/ideas/${this.props.idea.id}`,
+            {idea: idea}
+        )
+        .then(response => {
+            console.log(response)
+            this.props.updateIdea(response.data)
+        }) 
+        .catch(error => console.log(error))
+    }
+
     render () {
         return (
             <div className="tile">
-                <form>
+                <form onBlur={this.handleBlur}>
                     <input className="input" type="text" name = "title" placeholder="Enter a Title" value ={this.state.title} onChange={this.handleInput}/>
                     <textarea className="input" name="body" placeholder="Describe your idea" value = {this.state.body} onChange={this.handleInput}></textarea>
                 </form>
--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -31,6 +31,12 @@ class IdeasContainer extends Component {
         .catch(error => console.log(error))
     }
 
+    updateIdea = (idea) => {
+        const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+        const ideas = update(this.state.ideas, {[ideaIndex]: {$set: idea}})
+        this.setState({ideas: ideas})
+    }
+
     render() {
         return (
             <div>
@@ -41,7 +47,7 @@ class IdeasContainer extends Component {
                 </div>
                 {this.state.ideas.map((idea) => {
                     if(this.state.editingIdeaId === idea.id) {
-                        return(<IdeaForm idea={idea} key={idea.id} />)
+                        return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} />)
                     } else {
                         return(<Idea idea={idea} key={idea.id} />)
                     }
