54f64244b262bfafcb6624287802a5b5bbb0f380
Candice Davidson <cndragn@gmail.com>
2018-05-09 21:28:54 -0400

    map through api to display data

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -19,8 +19,15 @@ class IdeasContainer extends Component {
 
     render() {
         return (
-            <div className="">
-                Ideas
+            <div>
+                {this.state.ideas.map((idea) => {
+                    return(
+                        <div className="tile" key={idea.id}>
+                        <h4>{idea.title}</h4>
+                        <p>{idea.body}</p>
+                        </div>
+                    )
+                })}
             </div>  
         );
     }
