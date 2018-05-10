8b7a36d042931b6982fef17a8ef174471acafa7a
Candice Davidson <cndragn@gmail.com>
2018-05-09 21:39:37 -0400

    refactor idea into own compoenent

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import Idea from './Idea'
 
 class IdeasContainer extends Component {
     constructor(props) {
@@ -22,10 +23,7 @@ class IdeasContainer extends Component {
             <div>
                 {this.state.ideas.map((idea) => {
                     return(
-                        <div className="tile" key={idea.id}>
-                        <h4>{idea.title}</h4>
-                        <p>{idea.body}</p>
-                        </div>
+                        <Idea idea={idea} key={idea.id} />
                     )
                 })}
             </div>  
