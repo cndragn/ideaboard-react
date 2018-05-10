3e95f32f390e37545fa11308339807724dd1c1f6
Candice Davidson <cndragn@gmail.com>
2018-05-09 23:01:33 -0400

    make new idea form functional

--- src/components/IdeaForm.js
+++ src/components/IdeaForm.js
@@ -4,16 +4,21 @@ class IdeaForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            title: this.props.idea.title,
+            body: this.props.idea.body
         }
+    }
+
+    handleInput = (e) => {
+        this.setState({[e.target.name]: e.target.value})
     }
 
     render () {
         return (
             <div className="tile">
                 <form>
-                    <input className="input" type="text" name = "title" placeholder="Enter a Title" />
-                    <textarea className="input" name="body" placeholder="Describe your idea"></textarea>
+                    <input className="input" type="text" name = "title" placeholder="Enter a Title" value ={this.state.title} onChange={this.handleInput}/>
+                    <textarea className="input" name="body" placeholder="Describe your idea" value = {this.state.body} onChange={this.handleInput}></textarea>
                 </form>
             </div>
         );
